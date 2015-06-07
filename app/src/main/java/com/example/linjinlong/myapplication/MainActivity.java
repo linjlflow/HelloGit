fdb1f7d14a020e858cfeb5317d25088a31247c4f
linjlflow <linjlflow@gmail.com>
2015-06-07 14:48:00 +0800

    Merge branch 'master' of https://github.com/linjlflow/HelloGit
    # Conflicts:
    #	.gitignore

--- app/src/main/java/com/example/linjinlong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/linjinlong/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //此时提交就出问题了~~
     }
 
     @Override
